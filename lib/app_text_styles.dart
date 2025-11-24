d430d59f590e4cf2a03140d591653b0d9e4e54ce
Salonee Shrestha <saloneeshrestha35@gmail.com>
2025-11-24 09:29:00 +0545

    updated login file

---
+++ lib/app_text_styles.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  AppTextStyle._();
+
+  static TextStyle poppinsRegular = TextStyle(
+    fontFamily: 'Poppins',
+    fontSize: 12,
+    color: Colors.black,
+  );
+
+  static TextStyle poppinsMedium = TextStyle(
+    fontFamily: 'PoppinsMedium',
+    fontSize: 12,
+    color: Colors.black,
+  );
+
+  static TextStyle poppinsBold = TextStyle(
+    fontFamily: 'PoppinsBold',
+    fontSize: 12,
+    color: Colors.black,
+  );
+
+  static TextStyle poppinsExtraBold = TextStyle(
+    fontFamily: 'PoppinsExtraBold',
+    fontSize: 12,
+    color: Colors.black,
+  );
+
+  static TextStyle poppinsSemiBold = TextStyle(
+    fontFamily: 'PoppinsSemiBold',
+    fontSize: 12,
+    color: Colors.black,
+  );
+}
--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pinterest/app_text_styles.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,6 +9,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool isObscure = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,15 +28,72 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(height: 12),
             Text(
               'Welcome Back',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
+              style: AppTextStyle.poppinsBold.copyWith(fontSize: 28),
             ),
             Text(
               'Make it work, Make it right, Make it fast',
-              style: TextStyle(fontSize: 18, color: Colors.black),
+              style: AppTextStyle.poppinsSemiBold.copyWith(fontSize: 16),
+            ),
+            SizedBox(height: 30),
+            TextFormField(
+              style: AppTextStyle.poppinsRegular.copyWith(fontSize: 14),
+              keyboardType: TextInputType.emailAddress,
+              controller: emailController,
+              decoration: InputDecoration(
+                hintText: 'E-Mail',
+                hintStyle: AppTextStyle.poppinsRegular.copyWith(
+                  fontSize: 14,
+                  color: Colors.black.withOpacity(0.6),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 20,
+                ),
+                prefixIcon: Icon(Icons.person, size: 26),
+              ),
+            ),
+            SizedBox(height: 14),
+            TextFormField(
+              style: AppTextStyle.poppinsRegular.copyWith(fontSize: 14),
+              controller: passwordController,
+              obscureText: isObscure,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                hintStyle: AppTextStyle.poppinsRegular.copyWith(
+                  fontSize: 14,
+                  color: Colors.black.withOpacity(0.6),
+                ),
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey, width: 1),
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+                contentPadding: EdgeInsets.symmetric(
+                  horizontal: 10,
+                  vertical: 20,
+                ),
+                prefixIcon: Icon(Icons.fingerprint, size: 26),
+                suffixIcon: IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () {
+                    setState(() {
+                      isObscure = !isObscure;
+                    });
+                  },
+                  icon: Icon(isObscure ? Icons.visibility_off : Icons.visibility),
+                ),
+              ),
             ),
           ],
         ),
