88a007fe5ece4d4ce5d9c308785e614627e4f882
Salonee Shrestha <saloneeshrestha35@gmail.com>
2025-12-19 09:12:38 +0545

    added grid view

--- lib/modules/home/dashboard_view.dart
+++ lib/modules/home/dashboard_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pinterest/constants/app_text_styles.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
@@ -11,7 +12,48 @@ class _DashboardViewState extends State<DashboardView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard'),),
+      appBar: AppBar(
+        title: Text(
+          'Dashboard',
+          style: AppTextStyle.poppinsSemiBold.copyWith(fontSize: 24),
+        ),
+        toolbarHeight: 80,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Top Beaches',
+                style: AppTextStyle.poppinsMedium.copyWith(fontSize: 20),
+              ),
+              SizedBox(height: 16),
+              GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.blue, width: 2),
+                    ),
+                    padding: EdgeInsets.all(16),
+                  );
+                },
+                itemCount: 2,
+                primary: false,
+                shrinkWrap: true,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
